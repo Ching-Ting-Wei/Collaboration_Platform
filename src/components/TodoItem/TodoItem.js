fc66a6491d0ab0c827026f31949cc6b74c26a8d6
Ching-Ting-Wei <tim149557@gmail.com>
2023-11-23 19:18:23 +0800

    todo

---
+++ src/components/TodoItem/TodoItem.js
@@ -0,0 +1,116 @@
+import React from "react";
+import styled from "styled-components";
+
+const Container = styled.div`
+  border-radius: 20px;
+  box-shadow: 7px 9px 0px 0px #000;
+  background-color: rgba(0, 0, 0, 0.7);
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  padding: 0 31px 0 25px;
+
+  @media (max-width: 991px) {
+    padding: 0 20px;
+  }
+`;
+
+const Header = styled.div`
+  align-self: stretch;
+  display: flex;
+  margin-top: 28px;
+  align-items: center;
+  justify-content: space-between;
+  gap: 11px;
+
+  @media (max-width: 991px) {
+    max-width: 100%;
+    flex-wrap: wrap;
+  }
+`;
+
+const Title = styled.div`
+  color: var(--main-color, #fff8f0);
+  text-transform: uppercase;
+  margin: auto 0;
+  font: 400 24px/30px Caveat Brush, sans-serif;
+`;
+
+const Subtitle = styled.div`
+  color: #000;
+  white-space: nowrap;
+  border-radius: 10px;
+  background-color: var(--main-color, #fff8f0);
+  align-self: stretch;
+  flex-grow: 1;
+  align-items: start;
+  padding: 10px 15px;
+  font: 400 24px/30px Caveat Brush, sans-serif;
+
+  @media (max-width: 991px) {
+    white-space: initial;
+  }
+`;
+
+const TotalQuestions = styled.div`
+  color: var(--main-color, #fff8f0);
+  text-transform: uppercase;
+  align-self: stretch;
+  margin-top: 20px;
+  white-space: nowrap;
+  font: 400 24px/30px Caveat Brush, sans-serif;
+
+  @media (max-width: 991px) {
+    max-width: 100%;
+    white-space: initial;
+  }
+`;
+
+const Actions = styled.div`
+  display: flex;
+  width: 236px;
+  max-width: 100%;
+  justify-content: space-between;
+  gap: 20px;
+  margin: 25px 0 9px;
+`;
+
+const Button = styled.div`
+  color: #000;
+  text-align: center;
+  text-transform: uppercase;
+  white-space: nowrap;
+  border-radius: 20px;
+  box-shadow: 3px 4px 0px 0px #000;
+  background-color: var(--main-color, #fff8f0);
+  align-items: center;
+  padding: 9px 20px;
+  font: 400 20px/25px Caveat Brush, sans-serif;
+
+  @media (max-width: 991px) {
+    white-space: initial;
+  }
+`;
+
+const WhiteButton = styled(Button)`
+  color: black;
+  background-color: var(--main-color, #FF8811);
+`;
+
+
+export default function TodoItem(){
+
+    return(
+    <Container>
+        <Header>
+            <Title>作業名稱：</Title>
+            <Subtitle>dd hw02</Subtitle>
+        </Header>
+        <TotalQuestions>共有 5 大題</TotalQuestions>
+        <Actions>
+            <Button>返回</Button>
+            <WhiteButton>下一步</WhiteButton>
+        </Actions>
+    </Container>
+    );
+}
