39778ea0c2b8298b9f0711ae3a673708aafa854e
Ching-Ting-Wei <tim149557@gmail.com>
2023-12-07 19:48:37 +0800

    revise

--- src/pages/GroupPage/GroupPage.js
+++ src/pages/GroupPage/GroupPage.js
@@ -3,9 +3,37 @@ import Group from "../../components/Group/Group";
 import GroupItem from "../../components/Group/GroupItem";
 import UserHeader from "../../components/UserHeader";
 import {v4 as uuidv4} from 'uuid';
+import axios from '../../api/axios';
+import styled from "styled-components";
+const CHECK_URL = '/checkLoginStatus';
 uuidv4();
 
+const P = styled.p`
+  color: black;
+`;
+
 export default function GroupPage() {
+
+  const [loggedIn, setLoggedIn] = useState(false);
+  const [userData, setUserData] = useState(null);
+  
+  axios.get(CHECK_URL, { withCredentials: true })
+  .then(response => {
+    if (response.data.loggedIn) {
+      setLoggedIn(true)
+      console.log(response.data)
+      setUserData(response.data.user.user)
+      // 如果已登入，可以根據用戶資訊執行相應的操作
+    } else {
+      console.log('User is not logged in');
+      // 如果未登入，可以執行未登入的相應處理
+    }
+  })
+  .catch(error => {
+    console.error('Error checking login status:', error);
+    // 處理錯誤情況
+  });
+
   const [groups, setGroups] = useState([]);
 
   const addGroup = group => {
@@ -15,6 +43,11 @@ export default function GroupPage() {
   return (
     <>
       <UserHeader></UserHeader>
+      {loggedIn ? (
+        <P>Welcome to the dashboard!!! {userData} </P>
+      ) : (
+        <P>Please log in to access the dashboard</P>
+      )}
       <Group addGroup = {addGroup}/>
       {groups.map((group, index) => (
         <GroupItem task = {group} key = {index}/>
--- src/pages/UserPage/UserPage.js
+++ src/pages/UserPage/UserPage.js
@@ -1,8 +1,9 @@
-import React from "react";
+import React,{useEffect, useState} from "react";
 import Todo from "../../components/TodoItem";
 import styled from "styled-components";
-import { useState} from 'react';
 import UserHeader from "../../components/UserHeader";
+import axios from '../../api/axios';
+const CHECK_URL = '/checkLoginStatus';
 
 const Mask = styled.div`
   display: ${({ $show }) => ($show ? 'block' : 'none')};
@@ -25,10 +26,35 @@ const Button = styled.button`
   font-size: 16px;
 `;
 
+const P = styled.p`
+  color: black;
+`;
+
 
 export default function UserPage() {
   const [showMask, setShowMask] = useState(false);
+  const [loggedIn, setLoggedIn] = useState(false);
+  const [userData, setUserData] = useState(null);
 
+  axios.get(CHECK_URL, { withCredentials: true })
+  .then(response => {
+    if (response.data.loggedIn) {
+      setLoggedIn(true)
+      console.log(response.data)
+      setUserData(response.data.user.user)
+      // 如果已登入，可以根據用戶資訊執行相應的操作
+    } else {
+      console.log('User is not logged in');
+      // 如果未登入，可以執行未登入的相應處理
+    }
+  })
+  .catch(error => {
+    console.error('Error checking login status:', error);
+    // 處理錯誤情況
+  });
+
+  
+  
   const toggleMask = () => {
     setShowMask(!showMask);
   };
@@ -38,11 +64,15 @@ export default function UserPage() {
   };
 
 
-
   return (
-    <>
+    <> 
     <UserHeader></UserHeader>
     <div>
+      {loggedIn ? (
+        <P>Welcome to the dashboard!!! {userData} </P>
+      ) : (
+        <P>Please log in to access the dashboard</P>
+      )}
       <Button onClick={toggleMask}>Add HomeWork</Button>
       <Mask $show={showMask}>
 	  	  <Todo handleCloseMask={handleCloseMask}></Todo>
